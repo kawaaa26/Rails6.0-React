2c4a9ff99b201152c28281c17190efab80f8a41a
kawaaa26 <unic.almj@yahoo.com>
2019-09-13 20:17:26 +0900

    :zap: post functionalities implemented with Ajax.
    Closes #26

--- crud_front/src/Components/MainContainer.jsx
+++ crud_front/src/Components/MainContainer.jsx
@@ -1,9 +1,10 @@
-import React from 'react';
-import axios from 'axios';
+import React from 'react'
+import axios from 'axios'
 // import 'bootstrap/dist/css/bootstrap.css';
 // import 'bootstrap/dist/css/bootstrap-theme.css';
-import ProductsContainer from './ProductsContainer';
-import FormContainer from './FormContainer';
+import ProductsContainer from './ProductsContainer'
+import FormContainer from './FormContainer'
+import update from 'react-addons-update';
 
 export default class MainContainer extends React.Component {
   constructor(props) {
@@ -15,21 +16,30 @@ export default class MainContainer extends React.Component {
 
   componentDidMount() {
     axios.get('http://localhost:3001/products')
-      .then((results) => {
-        console.log(results)
-        this.setState({products: results.data})
-      })
+    .then((results) => {
+      this.setState({products: results.data})
+    })
+    .catch((data) =>{
+      console.log(data)
+    })
+  }
 
-      .catch((data) => {
-        console.log(data)
-      })
+  createProduct = (product) => {
+    axios.post('http://localhost:3001/products',{product: product} )
+    .then((response) => {
+      const newData = update(this.state.products, {$push:[response.data]})
+      this.setState({products: newData})
+    })
+    .catch((data) =>{
+      console.log(data)
+    })
   }
 
   render() {
     return (
       <div className='app-main'>
-        <FormContainer />
-        <ProductsContainer productData={ this.state.products } />
+        <FormContainer  hendleAdd={this.hendleAdd} createProduct={this.createProduct}/>
+        <ProductsContainer productData={this.state.products} deleateProduct={this.deleateProduct} updateProduct={this.updateProduct}/>
       </div>
     );
   }
